9067dcd99be17a8c1f007e09d35acc0e83b2764a
tsuroki <tsuroki0210@gmail.com>
2025-10-16 15:54:45 +0900

    251016 coding

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+const bnrSlide = new Swiper ('#main_wrap #topbnr', {
+    loop : true,
+    autoplay :  {delay : 6000}
+    })
+const bestProductSlide = new Swiper ('#main_wrap .best_product > #product-list', {
+    loop : true,
+    slidesPerView : 3.5,
+    })
