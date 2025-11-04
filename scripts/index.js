7edf3af2ee4e778373a0f441eb09f63cda338a93
tsuroki <tsuroki0210@gmail.com>
2025-11-04 18:11:32 +0900

    251104 html구조 수정

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,30 @@
 const bnrSlide = new Swiper ('#main_wrap #topbnr', {
     loop : true,
-    autoplay :  {delay : 6000}
+    autoplay :  {delay : 6000},
+    pagination:{
+        el:'.bnr-nav',
+        clickable:true,
+    },
     })
-const bestLineSlide = new Swiper('.best-list', {
+
+const bestSlide = new Swiper('.best_list', {
+    slidesPerView : 3.8,
+    spaceBetween : 50,
+    })
+
+const newSlide = new Swiper('.new_list', {
+    slidesPerView : 3.8,
+    spaceBetween : 50,
+    })
+
+const fwSlide = new Swiper('.fw_season_list ', {
+    slidesPerView : 1,
+    spaceBetween : 50,
+    })
+
+const whatsOnSlide = new Swiper('.wo-slide', {
     loop : true,
-    slidesPerView : 4,
+    slidesPerView : 1,
+    centeredSlides: true,
+    spaceBetween:50,
     })
